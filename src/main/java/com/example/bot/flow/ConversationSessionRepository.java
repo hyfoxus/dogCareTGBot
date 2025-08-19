8cee1ff44c8241d665c5208de4078ac1247140f5
hyfoxus <hyfoxus@gmail.com>
2025-08-19 19:12:13 +0300

    Fixed unimaginable amount of errors

--- src/main/java/com/example/bot/flow/ConversationSessionRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.bot.flow;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ConversationSessionRepository extends CrudRepository<ConversationSession, Long> {}
